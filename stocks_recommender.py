6b606ae51a75a6e11a400bb212646f06ed8278c4
prathmeshpalande <prathmeshpalande>
2022-02-13 16:35:18 +0530

    Added requirements.txt

--- stocks_recommender.py
+++ stocks_recommender.py
@@ -1,40 +1,40 @@
-from fastapi import FastAPI
-import uvicorn
+# from fastapi import FastAPI
+# import uvicorn
 
-from pymongo import MongoClient
-import pymongo
-from pydantic import BaseModel
+# from pymongo import MongoClient
+# import pymongo
+# from pydantic import BaseModel
 
-class RecommenderConfig(BaseModel):
-    'instrument_token': str
-    'interval': int
+# class RecommenderConfig(BaseModel):
+#     'instrument_token': str
+#     'interval': int
 
-class Recommenders(BaseModel):
-    'recommenders': list[RecommenderConfig]
+# class Recommenders(BaseModel):
+#     'recommenders': list[RecommenderConfig]
 
 
-app = FastAPI()
-client = MongoClient('mongodb://localhost:27017/')
-db = client['kiteconnect']
-stocks_actions_recommendations = db['stocks_actions_recommendations']
+# app = FastAPI()
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client['kiteconnect']
+# stocks_actions_recommendations = db['stocks_actions_recommendations']
 
-@app.get('/instruments')
-def get_instruments():
-    instruments_collection = db['instruments']
-    instruments = []
-    for instrument in instruments_collection.find():
-        del instrument['_id']
-        instruments.append(instrument)
-    return {'instruments': instruments}
+# @app.get('/instruments')
+# def get_instruments():
+#     instruments_collection = db['instruments']
+#     instruments = []
+#     for instrument in instruments_collection.find():
+#         del instrument['_id']
+#         instruments.append(instrument)
+#     return {'instruments': instruments}
 
-@app.post('/recommender')
-def create_recommender(recommenders: Recommenders):
-    recommenders_mongo = db['recommenders']
-    recommenders_mongo.insert_many(recommenders)
+# @app.post('/recommender')
+# def create_recommender(recommenders: Recommenders):
+#     recommenders_mongo = db['recommenders']
+#     recommenders_mongo.insert_many(recommenders)
 
-@app.get('/recommendations')
-def get_recommendations():
-    recommenders_mongo = db['recommenders']
-    recommendations = db['action_recommendations']
-    for recommender in recommenders_mongo.find():
-        recommender['']
+# @app.get('/recommendations')
+# def get_recommendations():
+#     recommenders_mongo = db['recommenders']
+#     recommendations = db['action_recommendations']
+#     for recommender in recommenders_mongo.find():
+#         recommender['']
